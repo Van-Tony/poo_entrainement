b3010896fc9d1704fee495911309d417270d3366
Van-Tony <tony.van.professional@gmail.com>
2025-01-30 18:43:50 +0100

    Suppression de plusieurs instructions, mis à jour des arguments lors de l'instance de Personnage, ajout de l'appel de la fonction afficherEtat() par l'objet tony

--- main.cpp
+++ main.cpp
@@ -3,13 +3,7 @@
 
 int main()
 {
-    Personnage tony("clavier", 200), lola(tony);
-    tony.attaquer(lola);
-    lola.boirePotionDeVie(20);
-    tony.attaquer(lola);
-    lola.attaquer(tony);
-
-    lola.changerArme("hache",40);
-    lola.attaquer(tony);
+    Personnage tony("tony");
+    tony.afficherEtat();
     return 0;
 }
