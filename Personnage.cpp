2689016193be096b42d7dc1c9a9143ca6b01763a
Van-Tony <tony.van.professional@gmail.com>
2025-01-30 17:05:11 +0100

    Suppression du constructeur de copie

--- Personnage.cpp
+++ Personnage.cpp
@@ -1,12 +1,7 @@
 #include <string>
-    //constructeur de copie
-Personnage::Personnage(Personnage const& autre) : m_nomArme(autre.m_nomArme), m_degatsArme(autre.m_degatsArme), m_ptDeVie(autre.m_ptDeVie), m_nbDeMana(autre.m_nbDeMana)
-    {
-
-    }
 
     // constructeur
-    Personnage::Personnage(std::string nomArme, int degatsArme, int ptDeVie) : m_nomArme(nomArme),m_degatsArme(degatsArme), m_nbDeMana(100), m_ptDeVie(ptDeVie)
+    Personnage::Personnage() : m_nomArme(nomArme),m_degatsArme(degatsArme), m_nbDeMana(100), m_ptDeVie(ptDeVie)
         {
 
         }
